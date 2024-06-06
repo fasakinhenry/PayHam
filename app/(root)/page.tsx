7e077e73dd27e7ff777c8e248e10b750656e7d00
Fasakin Henry <fasakinhenry@gmail.com>
2024-06-06 19:53:28 +0100

    feat: create Header component for main pages
    Create a reusable Header component for the main pages.
    
    0.0.3

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import HeaderBox from '@/components/HeaderBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Henry' };
+
   return (
-    <div>Home</div>
-  )
-}
-export default Home
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently'
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+export default Home;
